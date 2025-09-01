10c87fa587f8013131fdaa6a2eb20de96d744083
Hryhorii Seniv <hryhorii.seniv@javatar.pro>
2025-09-01 22:23:15 +0300

    Implemented task

--- task.sql
+++ task.sql
@@ -1,2 +1,5 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
+
+CREATE index idx_customers_email ON Customers (email);
+CREATE index idx_products_name ON Products (name);
