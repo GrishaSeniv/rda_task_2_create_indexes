2c763eedbc3657e4573458afbe2ea7ebd7c3c8c4
Hryhorii Seniv <hryhorii.seniv@javatar.pro>
2025-09-01 22:34:20 +0300

    fixed script

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
 
-CREATE index 'Email' ON Customers (email);
-CREATE index 'Name' ON Products (name);
+CREATE INDEX `Email` ON Customers (email);
+CREATE INDEX `Name` ON Products (name);
